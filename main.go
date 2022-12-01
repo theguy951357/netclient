0b7c7437dcb2b0d7fccee304d4f03a5b35a7a2bf
Anish Mukherjee <anish.mukherjee1996@gmail.com>
2022-12-01 18:31:54 +0530

    reinstall daemon

--- main.go
+++ main.go
@@ -10,6 +10,7 @@ import (
 	"github.com/blang/semver"
 	"github.com/gravitl/netclient/cmd"
 	"github.com/gravitl/netclient/config"
+	"github.com/gravitl/netclient/daemon"
 	"github.com/rhysd/go-github-selfupdate/selfupdate"
 )
 
@@ -26,6 +27,19 @@ func autoUpdate() {
 	if !latest.Version.Equals(v) {
 		log.Println("Successfully updated to version", latest.Version)
 		log.Println("Release notes:\n", latest.ReleaseNotes)
+		// reboot daemon
+		if err := daemon.Stop(); err != nil {
+			log.Println("Error encountered while stopping daemon:", err)
+			return
+		}
+		if err := daemon.InstallDaemon(); err != nil {
+			log.Println("Error encountered while installing daemon:", err)
+			return
+		}
+		if err := daemon.Start(); err != nil {
+			log.Println("Error encountered while starting daemon:", err)
+			return
+		}
 	}
 }
 
